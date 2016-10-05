1e72c9fc4934ea5409f1fa77f2fd89899ddb18da
affinehat <krs@umd.edu>
2016-10-05 07:43:12 -0400

    Test hardcoding back/forward buttons by id

--- gmail-shortcuts.js
+++ gmail-shortcuts.js
@@ -3,11 +3,18 @@
 // @namespace   affinehat.net
 // @description Custom shortcuts for Gmail
 // @include     https://mail.google.com/mail/*
-// @version     0.1.0
+// @version     0.1.1
 // @grant       none
 // ==/UserScript==
 
+var back = document.getElementById(':ip');
+var forward = document.getElementById(':iq');
+
 function keyHandler(event) {
+  if (event.charCode === 104) { // 'h' pressed
+    // simulate click...
+  }
+
   if (event.charCode === 108) { // 'l' pressed
     event.stopPropagation();
   }
